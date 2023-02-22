4cea9e5f5dec3ca2766d0da71696922829a5c76e
Bar Barina <itsbarbarina@gmail.com>
2023-02-22 15:57:03 +0200

    added api key

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -39,7 +39,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyAkv6uHzL1yR1owAMmi_Gg-TOS9H-RVNbs' //Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
